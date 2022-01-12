70394f38386a6bd6258766502e0c853cfaa53c93
zrta2480 <96914273+zrta2480@users.noreply.github.com>
2022-01-12 16:40:10 +0800

    Add files via upload

--- deleteemployee.php
+++ deleteemployee.php
@@ -41,7 +41,7 @@ if(!isset($_SESSION['user']))
                     $old_last = "[your last name]";
                     $old_first = "[your first name]";
                     $old_position = "[your employee position]";
-                    $old_type = "[your employee type]";
+                    //$old_type = "[your employee type]";
                     $submitted_ID = $_GET['employeeID'];
                     $sql = "SELECT * FROM tblemployees";
                     $result = mysqli_query($con, $sql);
@@ -55,19 +55,19 @@ if(!isset($_SESSION['user']))
                             $old_last = $row['fldlastname'];
                             $old_first = $row['fldfirstname'];
                             $old_position = $row['fldposition'];
-                            $old_type = $row['fldemployeetype']; 
+                            //$old_type = $row['fldemployeetype']; 
                         }
                     }
 
                     if($verify_ID)
                     {
+                        echo "<form action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post'>";
                         echo "Employee ID: <input type='text' value='". $submitted_ID ."' readonly><br />";
                         echo "Last Name: <input type='text' value='" . $old_last . "' readonly><br />";
                         echo "First Name: <input type='text' value='" . $old_first . "' readonly><br />";
                         echo "Job Position: <input type='text' value='" . $old_position . "' readonly><br />";
-                        echo "Employee Type: <input type='text' value='" . $old_type . "' readonly><br />";
+                        //echo "Employee Type: <input type='text' value='" . $old_type . "' readonly><br />";
                         echo "<hr>";
-                        echo "<form action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post'>";
                         echo "<input type='hidden' name='selectedEmployeeID' value='" . $submitted_ID . "' />";
                         echo "<input type='submit' name='submit_delete_employee' value='Delete Employee' />";
                         echo "</form>";
